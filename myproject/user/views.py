7ed9b428674cbb23a3413daca7b2f1324cf13f32
yamhokim <yoonho.luke.kim@gmail.com>
2024-10-12 15:26:05 -0400

    added logout url and view

--- myproject/user/views.py
+++ myproject/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def register_view(request):
@@ -22,3 +22,10 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "user/login.html", { "form": form })
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
+    
